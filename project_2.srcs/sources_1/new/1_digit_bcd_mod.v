854a1628d628b56a72b7b373b36e98e1afff5508
AliMarashian <45737450+AliMarashian@users.noreply.github.com>
2020-10-20 12:02:42 +0330

    added bcd_1_digit_mod_3

---
+++ project_2.srcs/sources_1/new/1_digit_bcd_mod.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 10/20/2020 10:54:27 AM
+// Design Name: 
+// Module Name: one_digit_bcd_mod_3
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module one_digit_bcd_mod_3(
+
+    );
+endmodule
