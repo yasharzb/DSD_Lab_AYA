05cd8336d75ab88044bc1e27ef76c3c3a090fedd
yasharzb <45389623+yasharzb@users.noreply.github.com>
2020-10-25 15:36:21 +0330

    Initial files added

---
+++ Ex_2/room/room.srcs/sources_1/new/room_man.v
@@ -0,0 +1,30 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 10/25/2020 03:23:45 PM
+// Design Name: 
+// Module Name: room_man
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module room_man( 
+    input clk,
+    input in,
+    input out,
+    input ent,
+    output open
+    );
+endmodule
